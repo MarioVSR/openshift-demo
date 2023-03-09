394ed6cc211dae714017948441f7e478927ae308
Villanueva Sanchez Rojas <mvillanuevasanche@deloitte.com>
2023-03-09 13:00:19 -0600

    first commit

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hi from demo app";
+    }
+}
